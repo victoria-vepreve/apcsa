93b39908df8dbbbef37cfd2377efff86f4d5ae66
victoria-vepreve <victoriav88@nycstudents.net>
2024-09-11 13:14:52 +0000

    main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Numbers");
+        int j = 1;
+        double k = 2;
+        boolean l = true;
+        System.out.println(j + k);
+        j = j + 1;
+        final int m = 3;
+        System.out.println(j + m + " " + l);
+
+    }
+}
